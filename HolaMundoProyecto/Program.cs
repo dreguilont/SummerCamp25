60ed93175aac64d764cc19f5942841dba71cecab
dreguilont <dreguilont@gmail.com>
2025-07-16 11:36:17 +0200

    Primer Hola Mundo

---
+++ HolaMundoProyecto/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("--------------------------");
+
+        Console.WriteLine("Hello, World!");
+
+
+        Console.WriteLine("                                    _       ");
+          Console.WriteLine(" ___ ___   ___ __ _        ___ ___ | | __ _ ")   ;
+          Console.WriteLine(" / __/ _ \\ / __/ _` |_____ / __/ _ \\| |/ _` |");
+          Console.WriteLine("| (_| (_) | (_| (_| |_____| (_| (_) | | (_| |");
+        Console.WriteLine(" \\___\\___/ \\___\\__,_|     \\___\\___/|_|\\__,_|");
+        Console.WriteLine("soft drink - coke - The Coca-Cola company");
+    }
+}
